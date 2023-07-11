fc1da1a752d31b465e1ae978279a6f4cbc8ef39b
Gowtham <112604105+Gowtham152003@users.noreply.github.com>
2023-07-11 20:26:43 +0530

    Even_series_sum.c
    Enter the maximum value:20
    The even number lists are:
    2       4       6       8       10      12      14      16      18      20
    The sum of even number series:110

---
+++ Even_series_sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	printf("Enter the maximum value:");
+	scanf("%d",&n);
+	printf("The even number lists are:\n");
+	for(i=1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d\t",i);
+			sum=sum+i;
+		}
+	}
+	printf("\nThe sum of even number series:%d",sum);
+	return 0;
+}
